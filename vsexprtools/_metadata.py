f5dfa41adfcb68430a3ef2a667fffcbb64083a63
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2023-01-04 14:56:21 +0100

    Bump minor version for hotfix

--- vsexprtools/_metadata.py
+++ vsexprtools/_metadata.py
@@ -1,6 +1,6 @@
 """VapourSynth functions and helpers for writing RPN expressions."""
 
-__version__ = '1.3.1'
+__version__ = '1.3.2'
 
 __author__ = 'Setsugen no ao <setsugen@setsugen.dev>'
 __maintainer__ = __author__
