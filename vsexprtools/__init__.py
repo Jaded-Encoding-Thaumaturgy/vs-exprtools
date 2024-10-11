a116abf13fd386aee9a369d4b7912bfbee4b7b9e
LightArrowsEXE <Lightarrowsreboot@gmail.com>
2024-10-11 21:40:01 +0200

    `__init__`: top level noqa

--- vsexprtools/__init__.py
+++ vsexprtools/__init__.py
@@ -1,6 +1,8 @@
-from .exprop import *  # noqa: F401, F403
-from .funcs import *  # noqa: F401, F403
-from .manager import *  # noqa: F401, F403
-from .operators import *  # noqa: F401, F403
-from .util import *  # noqa: F401, F403
-from .variables import *  # noqa: F401, F403
+# ruff: noqa: F401, F403
+
+from .exprop import *
+from .funcs import *
+from .manager import *
+from .operators import *
+from .util import *
+from .variables import *
